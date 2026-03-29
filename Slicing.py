d4ab35217ce617187bb53ca67ff1d5339b78a583
Yashwant <yashwantmorabad52@gmail.com>
2026-03-29 16:25:00 +0530

    Done

---
+++ Slicing.py
@@ -0,0 +1,11 @@
+#Slicing
+import numpy as np
+x=np.array([1,2,3,4,5])
+print(x[0:])
+
+b=np.array([[1,2,3,4,5],[6,7,8,9,10],[5,4,3,2,1],[10,9,8,7,6]])
+print(b[0:2,0])
+print(b[0,0:2])
+print(b[0:3,0:3])
+
+
